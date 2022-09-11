af55df0d4de25e99f7b64b6dfa0f65a7dc4e9409
lokesh9999b <lokesh_mcr@srkrec.edu.in>
2022-09-11 22:29:00 +0530

    Lokesh Update

---
+++ photos.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="zxx">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Themezhub">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Aicte -Ideal Lab </title>
+		 
+        <!-- Custom CSS -->
+        <link href="assets/css/styles.css" rel="stylesheet">
+		<link  href="assets/css/leaderboard.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    </head>
+	
+    <body>
+
+        <?php
+		include 'menu.php';
+		?>
+			<!-- End Navigation -->
+			<div class="clearfix"></div>
+			<!-- ============================================================== -->
+			<!-- Top header  -->
+			<!-- ============================================================== -->
+
+			
+			<!-- ============================ Hero Banner  Start================================== -->
+			<div class="hero_banner image-cover image_bottom">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-9 col-md-10 col-sm-12">
+							<div class="simple-search-wrap">
+								<div class="hero_search-2 text-center">
+				
+				<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+					<div class="carousel-inner">
+					  <div class="carousel-item active">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					  <div class="carousel-item">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					  <div class="carousel-item">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+					  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					  <span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+					  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					  <span class="visually-hidden">Next</span>
+					</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	</div>
+	</div>
+			<!-- ============================ Hero Banner End ================================== -->
+			<!-- ================================ Tag Award ================================ -->
+			<!-- <section class="p-0">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="crp_box fl_color ovr_top">
+								<div class="row align-items-center">
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-journal-whills"></i></div>
+											<div class="dro_142">
+												<h6>200+ Students</h6>
+												<p>Body of Aicete ideal Lab</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-business-time"></i></div>
+											<div class="dro_142">
+												<h6>Lifetime Access</h6>
+												<p>Duis aute irure dolor in voluptate velit esse cillum labore .</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-user-shield"></i></div>
+											<div class="dro_142">
+												<h6>800k+ Enrolled</h6>
+												<p>Duis aute irure dolor in voluptate velit esse cillum labore .</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+			</section> -->
+			<!-- ================================ Tag Award ================================ -->
+			
+			<!-- ============================ Our Awards Start ================================== -->
+			<h1 class="row justify-content-center"> Gallery</h1>
+			<div class="row justify-content-center">
+			<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+							<div class="blg_grid_box">
+								<div class="blg_grid_thumb">
+									<a href="assets/img/aicte1.jpg"><img src="assets/img/aicte1.jpg" class="img-fluid" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_foot">
+									<div class="crs_flex">
+										<div class="crs_fl_first">
+											<div class="crs_tutor">
+												<div class="crs_tutor_thumb"><a href="instructor-detail.html"><img src="" class="img-fluid circle" alt=""></a></div>
+											</div>
+										</div>
+										<div class="crs_fl_last">
+											<div class="foot_list_info">
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+                   <!-- picture 2-->
+						<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+							<div class="blg_grid_box">
+								<div class="blg_grid_thumb">
+									<a href="assets/img/aicte3.jpg"><img src="assets/img/aicte3.jpg" class="img-fluid" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_foot">
+									<div class="crs_flex">
+										<div class="crs_fl_first">
+											<div class="crs_tutor">
+												<div class="crs_tutor_thumb"><a href="instructor-detail.html"><img src="" class="img-fluid circle" alt=""></a></div>
+											</div>
+										</div>
+										<div class="crs_fl_last">
+											<div class="foot_list_info">
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+				<!--picture 3-->
+				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+							<div class="blg_grid_box">
+								<div class="blg_grid_thumb">
+									<a href="assets/img/aicte2.jpg"><img src="assets/img/aicte2.jpg" class="img-fluid" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_foot">
+									<div class="crs_flex">
+										<div class="crs_fl_first">
+											<div class="crs_tutor">
+												<div class="crs_tutor_thumb"><a href="instructor-detail.html"><img src="" class="img-fluid circle" alt=""></a></div>
+											</div>
+										</div>
+										<div class="crs_fl_last">
+											<div class="foot_list_info">
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+                 </div>
+				 <!--picture -->
+				 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+							<div class="blg_grid_box">
+								<div class="blg_grid_thumb">
+									<a href="assets/img/woodcutter.jpg"><img src="assets/img/woodcutter.jpg" class="img-fluid" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_foot">
+									<div class="crs_flex">
+										<div class="crs_fl_first">
+											<div class="crs_tutor">
+												<div class="crs_tutor_thumb"><a href="instructor-detail.html"><img src="" class="img-fluid circle" alt=""></a></div>
+											</div>
+										</div>
+										<div class="crs_fl_last">
+											<div class="foot_list_info">
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+                 </div>
+
+<!-- ============================ Footer Start ================================== -->
+			<?php
+			include 'footer.php';
+			?>
+			<!-- ============================ Footer End ================================== -->
+			
+			<!-- Log In Modal -->
+			
+			<!-- End Modal -->
+			
+			<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+			
+
+		</div>
+		<!-- ============================================================== -->
+		<!-- End Wrapper -->
+		<!-- ============================================================== -->
+
+		<!-- ============================================================== -->
+		<!-- All Jquery -->
+		<!-- ============================================================== -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/select2.min.js"></script>
+		<script src="assets/js/slick.js"></script>
+		<script src="assets/js/moment.min.js"></script>
+		<script src="assets/js/daterangepicker.js"></script> 
+		<script src="assets/js/summernote.min.js"></script>
+		<script src="assets/js/metisMenu.min.js"></script>	
+		<script src="assets/js/custom.js"></script>
+		<!-- ============================================================== -->
+		<!-- This page plugins -->
+		<!-- ============================================================== -->		
+
+	</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="zxx">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Themezhub">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Aicte -Ideal Lab </title>
+		 
+        <!-- Custom CSS -->
+        <link href="assets/css/styles.css" rel="stylesheet">
+		<link  href="assets/css/leaderboard.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    </head>
+	
+    <body>
+
+        <?php
+		include 'menu.php';
+		?>
+			<!-- End Navigation -->
+			<div class="clearfix"></div>
+			<!-- ============================================================== -->
+			<!-- Top header  -->
+			<!-- ============================================================== -->
+			
+			
+			<!-- ============================ Hero Banner  Start================================== -->
+		<div class="hero_banner image-cover image_bottom">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-9 col-md-10 col-sm-12">
+							<div class="simple-search-wrap">
+								<div class="hero_search-2 text-center">
+				
+				<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+					<div class="carousel-inner">
+					  <div class="carousel-item active">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					  <div class="carousel-item">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					  <div class="carousel-item">
+						<img src="https://pranavtej.github.io/aicte-ideal-lab/assets/img/slider.png" class="d-block w-100" alt="...">
+					  </div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+					  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					  <span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+					  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					  <span class="visually-hidden">Next</span>
+					</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	</div>
+	</div>
+			<!-- ============================ Hero Banner End ================================== -->
+			<!-- ================================ Tag Award ================================ -->
+			<!---- <section class="p-0">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="crp_box fl_color ovr_top">
+								<div class="row align-items-center">
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-journal-whills"></i></div>
+											<div class="dro_142">
+												<h6>200+ Students</h6>
+												<p>Body of Aicete ideal Lab</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-business-time"></i></div>
+											<div class="dro_142">
+												<h6>Lifetime Access</h6>
+												<p>Duis aute irure dolor in voluptate velit esse cillum labore .</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+										<div class="dro_140">
+											<div class="dro_141 de"><i class="fa fa-user-shield"></i></div>
+											<div class="dro_142">
+												<h6>800k+ Enrolled</h6>
+												<p>Duis aute irure dolor in voluptate velit esse cillum labore .</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+			</section> -->
+			<!-- ================================ Tag Award ================================ -->
+			
+			<!-- ============================ Our Awards Start ================================== -->
+			<!----<section class="p-0">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="crp_box ovr_top">
+								<div class="row align-items-center m-0">
+									<div class="col-xl-2 col-lg-3 col-md-4 col-sm-12">
+										<div class="crp_tags"><h6>Over 700+ Cources in one place</h6></div>
+									</div>
+									<div class="col-xl-10 col-lg-9 col-md-8 col-sm-12">
+										<div class="part_rcp">
+											<ul>
+												<li><div class="crp_img"><img src="assets/img/lg-1.png" class="img-fluid" alt=""></div></li>
+												<li><div class="crp_img"><img src="assets/img/lg-5.png" class="img-fluid" alt=""></div></li>
+												<li><div class="crp_img"><img src="assets/img/lg-6.png" class="img-fluid" alt=""></div></li>
+												<li><div class="crp_img"><img src="assets/img/lg-7.png" class="img-fluid" alt=""></div></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>-->
+			<!-- ================================Patent cards ================================-->
+            <section class="gray">
+				<div class="container">
+			<div class="row justify-content-center">
+                
+						
+						<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                            <div class="sec-heading center">
+                                <h2> <span class="theme-cl">Projects List</span></h2>
+                                <p>AICTE IDEA labs to foster creativity, experiential
+                                    learning on campus patents 
+                                    </p>
+                            </div>
+							<div class="crs_grid_list">
+								
+								<div class="crs_grid_list_thumb">
+									<a href="course-detail.html"><img src="assets/img/project.png.png" class="img-fluid rounded" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_list_caption">
+									<div class="crs_lt_101">
+										<span class="est st_1">project Id : 100023</span>
+										<!-- <span class="est st_2">Technology</span> -->
+									</div>
+                                    
+									<div class="crs_lt_102">
+										<h2 ="crs_tit">Project name</h2>
+										<h6>Published By :<span class="crs_auth"> Publisher Nmae</span></h6>
+                                       <h6> Publish date : <span class="crs_auth">10/04/2003</span></h6>
+                                        <h6>Author's :<span class="crs_auth"> 1. Names </span><br></h6>
+                                       <h6> Approval By  : <span class="crs_auth"> 1. Names </span></h6>
+
+                                        
+									</div>
+									
+									
+								</div>
+								
+							</div>
+							<div class="crs_grid_list">
+								
+								<div class="crs_grid_list_thumb">
+									<a href="course-detail.html"><img src="assets/img/project.png.png" class="img-fluid rounded" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_list_caption">
+									<div class="crs_lt_101">
+										<span class="est st_1">project Id : 100023</span>
+										<!-- <span class="est st_2">Technology</span> -->
+									</div>
+                                    
+									<div class="crs_lt_102">
+										<h2 ="crs_tit">Project name</h2>
+										<h6>Published By :<span class="crs_auth"> Publisher Nmae</span></h6>
+                                       <h6> Publish date : <span class="crs_auth">10/04/2003</span></h6>
+                                        <h6>Author's :<span class="crs_auth"> 1. Names </span><br></h6>
+                                       <h6> Approval By  : <span class="crs_auth"> 1. Names </span></h6>
+
+                                        
+									</div>
+									
+									
+								</div>
+								
+							</div>
+							<div class="crs_grid_list">
+								
+								<div class="crs_grid_list_thumb">
+									<a href="course-detail.html"><img src="assets/img/project.png.png" class="img-fluid rounded" alt=""></a>
+								</div>
+								
+								<div class="crs_grid_list_caption">
+									<div class="crs_lt_101">
+										<span class="est st_1">project Id : 100023</span>
+										<!-- <span class="est st_2">Technology</span> -->
+									</div>
+                                    
+									<div class="crs_lt_102">
+										<h2 ="crs_tit">Project name</h2>
+										<h6>Published By :<span class="crs_auth"> Publisher Nmae</span></h6>
+                                       <h6> Publish date : <span class="crs_auth">10/04/2003</span></h6>
+                                        <h6>Author's :<span class="crs_auth"> 1. Names </span><br></h6>
+                                       <h6> Approval By  : <span class="crs_auth"> 1. Names </span></h6>
+
+                                        
+									</div>
+									
+									
+								</div>
+								
+							</div>
+						</div>
+              </div>
+                </div>
+                </section>
+
+<!-- ============================ Footer Start ================================== -->
+			<?php
+			include 'footer.php';
+			?>
+			<!-- ============================ Footer End ================================== -->
+			
+			<!-- Log In Modal -->
+			<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="loginmodal" aria-hidden="true">
+				<div class="modal-dialog modal-xl login-pop-form" role="document">
+					<div class="modal-content overli" id="loginmodal">
+						<div class="modal-header">
+							<h5 class="modal-title">Login Your Account</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							  <span aria-hidden="true"><i class="fas fa-times-circle"></i></span>
+							</button>
+						  </div>
+						<div class="modal-body">
+							<div class="login-form">
+								<form>
+								
+									<div class="form-group">
+										<label>User Name</label>
+										<div class="input-with-icon">
+											<input type="text" class="form-control" placeholder="User or email">
+											<i class="ti-user"></i>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<label>Password</label>
+										<div class="input-with-icon">
+											<input type="password" class="form-control" placeholder="*******">
+											<i class="ti-unlock"></i>
+										</div>
+									</div>
+									
+									<div class="form-group row">
+										<div class="col-xl-4 col-lg-4 col-4">
+											<input id="admin" class="checkbox-custom" name="admin" type="checkbox">
+											<label for="admin" class="checkbox-custom-label">Admin</label>
+										</div>
+										<div class="col-xl-4 col-lg-4 col-4">
+											<input id="student" class="checkbox-custom" name="student" type="checkbox" checked="">
+											<label for="student" class="checkbox-custom-label">Student</label>
+										</div>
+										<div class="col-xl-4 col-lg-4 col-4">
+											<input id="instructor" class="checkbox-custom" name="instructor" type="checkbox">
+											<label for="instructor" class="checkbox-custom-label">Tutors</label>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<button type="submit" class="btn btn-md full-width theme-bg text-white">Login</button>
+									</div>
+									
+									<div class="rcs_log_125 pt-2 pb-3">
+										<span>Or Login with Social Info</span>
+									</div>
+									<div class="rcs_log_126 full">
+										<ul class="social_log_45 row">
+											<li class="col-xl-4 col-lg-4 col-md-4 col-4"><a href="javascript:void(0);" class="sl_btn"><i class="ti-facebook text-info"></i>Facebook</a></li>
+											<li class="col-xl-4 col-lg-4 col-md-4 col-4"><a href="javascript:void(0);" class="sl_btn"><i class="ti-google text-danger"></i>Google</a></li>
+											<li class="col-xl-4 col-lg-4 col-md-4 col-4"><a href="javascript:void(0);" class="sl_btn"><i class="ti-twitter theme-cl"></i>Twitter</a></li>
+										</ul>
+									</div>
+								
+								</form>
+							</div>
+						</div>
+						<div class="crs_log__footer d-flex justify-content-between mt-0">
+							<div class="fhg_45"><p class="musrt">Don't have account? <a href="signup.html" class="theme-cl">SignUp</a></p></div>
+							<div class="fhg_45"><p class="musrt"><a href="forgot.html" class="text-danger">Forgot Password?</a></p></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Modal -->
+			
+			<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+			
+
+		</div>
+		<!-- ============================================================== -->
+		<!-- End Wrapper -->
+		<!-- ============================================================== -->
+
+		<!-- ============================================================== -->
+		<!-- All Jquery -->
+		<!-- ============================================================== -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/select2.min.js"></script>
+		<script src="assets/js/slick.js"></script>
+		<script src="assets/js/moment.min.js"></script>
+		<script src="assets/js/daterangepicker.js"></script> 
+		<script src="assets/js/summernote.min.js"></script>
+		<script src="assets/js/metisMenu.min.js"></script>	
+		<script src="assets/js/custom.js"></script>
+		<!-- ============================================================== -->
+		<!-- This page plugins -->
+		<!-- ============================================================== -->		
+
+	</body>
+</html>
